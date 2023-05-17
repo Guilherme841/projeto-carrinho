29922cab14cef11bfb6f3d8e6c53841d8817a2b7
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-05-17 19:14:55 -0300

    update

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,28 @@
+// let divProduto = document.createElement("div");
+// divProduto.setAttribute("class", "containerProduto")
+// main.appendChild(divProduto)
+
+// const main = document.getElementsByTagName("main")[0];
+
 let nomeElemento = localStorage.getItem("nomeElemento");
 let imagemElemento = localStorage.getItem("imagemElemento");
 let preçoElemento = localStorage.getItem("preçoElemento");
-const main = document.getElementsByTagName("main")[0];
 
 console.log(nomeElemento)
 console.log(imagemElemento)
 console.log(preçoElemento)
 
-let divProduto = document.createElement("div");
-divProduto.setAttribute("class", "containerProduto")
-main.appendChild(divProduto)
+const tabela = document.getElementById("tabela")
 
-// divProduto.innerHTML = `
-//   <p id="nome">${nomeElemento}</p>
-//   <img src="${imagemElemento}" alt="${nomeElemento}">
-//   <p>${preçoElemento}</p>
-// `;
+let thElemento = document.createElement("th");
+thElemento.textContent = "Produto"
+tabela.appendChild(thElemento);
+
+let trElemento = document.createElement("tr");
+trElemento.setAttribute("id", "tr")
+tabela.appendChild(trElemento);
+
+const trSeleçao = document.getElementById("tr");
+
+let tdElemento = document.createElement("td");
+trSeleçao.appendChild(tdElemento);
