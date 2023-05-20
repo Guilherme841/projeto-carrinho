968d8cb071967fac71caf2802be1740f745878d0
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-05-20 20:35:09 -0300

    update

--- js/cart.js
+++ js/cart.js
@@ -14,24 +14,57 @@ console.log(preçoElemento);
 
 const tabela = document.getElementById("tabela");
 
+let tbody = document.createElement("tboby");
+tbody.setAttribute("id", "tbodyId");
+tabela.appendChild(tbody);
+
+const tbodySeleçao = document.getElementById("tbodyId");
+
 let thElemento = document.createElement("th");
 thElemento.textContent = "Produto";
-tabela.appendChild(thElemento);
+tbodySeleçao.appendChild(thElemento);
 
 let thElemento2 = document.createElement("th");
-thElemento2.textContent = "teste";
-thElemento2.setAttribute("id", "teste")
-tabela.appendChild(thElemento2);
+thElemento2.textContent = "Preço";
+thElemento2.setAttribute("id", "preço");
+tbodySeleçao.appendChild(thElemento2);
+
+let thElemento3 = document.createElement("th");
+thElemento3.textContent = "qntd";
+thElemento3.setAttribute("id", "qntd");
+tbodySeleçao.appendChild(thElemento3);
+
+let thElemento4 = document.createElement("th");
+thElemento4.textContent = "Delete";
+thElemento4.setAttribute("id", "thVazio");
+tbodySeleçao.appendChild(thElemento4);
+
+const thElemento2Seleçao = document.getElementById("preço");
 
 let trElemento = document.createElement("tr");
 trElemento.setAttribute("id", "tr");
-tabela.appendChild(trElemento);
+tbodySeleçao.appendChild(trElemento);
 
 const trSeleçao = document.getElementById("tr");
 
 let tdElemento = document.createElement("td");
 tdElemento.setAttribute("id", "td");
 trSeleçao.appendChild(tdElemento);
+
+let tdElemento2 = document.createElement("td");
+tdElemento2.setAttribute("class", "tds");
+tdElemento2.textContent = "Preço";
+trSeleçao.appendChild(tdElemento2);
+
+let tdElemento3 = document.createElement("td");
+tdElemento3.setAttribute("class", "tds");
+tdElemento3.textContent = "Qntd";
+trSeleçao.appendChild(tdElemento3);
+
+let tdElemento4 = document.createElement("td");
+tdElemento4.setAttribute("class", "tds");
+tdElemento4.textContent = "Delete";
+trSeleçao.appendChild(tdElemento4);
 
 const tdSeleçao = document.getElementById("td");
 
@@ -42,6 +75,9 @@ tdSeleçao.appendChild(divContainerImagemNome);
 const divContainerImagemNomeSeleçao = document.getElementById(
   "containerImagemNome"
 );
+
+// let divContainerBotoes = document.createElement("div");
+// divContainerImagemNome.appendChild(divContainerBotoes);
 
 let divImagem = document.createElement("div");
 divImagem.setAttribute("id", "imagem");
@@ -60,6 +96,10 @@ divContainerImagemNomeSeleçao.appendChild(divContainerQuantidadeDelete);
 const divContainerQuantidadeDeleteSeleçao = document.getElementById(
   "containerQuantidadeDelete"
 );
+
+let divDelete = document.createElement("div");
+divDelete.setAttribute("id", "containerDelete");
+divContainerImagemNome.appendChild(divDelete);
 
 let buttonMenos = document.createElement("button");
 buttonMenos.setAttribute("id", "menos");
@@ -88,7 +128,7 @@ buttonMaisSeleçao.appendChild(img3);
 
 let buttonDelete = document.createElement("button");
 buttonDelete.setAttribute("id", "delete");
-divContainerQuantidadeDeleteSeleçao.appendChild(buttonDelete);
+divDelete.appendChild(buttonDelete);
 
 const buttonDeleteSeleçao = document.getElementById("delete");
 
@@ -96,16 +136,48 @@ let img4 = document.createElement("img");
 img4.src = "imagens/delete.png";
 buttonDeleteSeleçao.appendChild(img4);
 
-let divContainerNomePreço = document.createElement("div")
+let divContainerNomePreço = document.createElement("div");
 divContainerNomePreço.setAttribute("id", "containerNomePreço");
 tdSeleçao.appendChild(divContainerNomePreço);
 
-const divContainerNomePreçoSeleçao = document.getElementById("containerNomePreço");
+const divContainerNomePreçoSeleçao =
+  document.getElementById("containerNomePreço");
 
 let paragrafo = document.createElement("p");
 paragrafo.textContent = "Boné";
 divContainerNomePreçoSeleçao.appendChild(paragrafo);
 
 let paragrafo2 = document.createElement("p");
+paragrafo2.setAttribute("id", "paragrafoPreço");
 paragrafo2.textContent = "preço: 31,90";
 divContainerNomePreçoSeleçao.appendChild(paragrafo2);
+
+function aplicarMedia() {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    paragrafo2.style.display = "none";
+    const paragrafo2Seleçao = document.getElementById("paragrafoPreço");
+    tdElemento2.textContent = paragrafo2Seleçao.innerHTML;
+
+    divContainerQuantidadeDeleteSeleçao.style.display = "none";
+    tdElemento3.innerHTML = divContainerQuantidadeDeleteSeleçao.innerHTML;
+
+    divDelete.style.display = "none";
+    tdElemento4.innerHTML = divDelete.innerHTML;
+  } else {
+    
+    paragrafo2.style.display = "block";
+    divContainerQuantidadeDeleteSeleçao.style.display = "block";
+    divDelete.style.display = "block";
+    divContainerQuantidadeDeleteSeleçao.appendChild(buttonMenos);
+    divContainerQuantidadeDeleteSeleçao.appendChild(span);
+    divContainerQuantidadeDeleteSeleçao.appendChild(buttonMais);
+    divContainerQuantidadeDeleteSeleçao.appendChild(buttonDelete);
+    divContainerQuantidadeDeleteSeleçao.style.display = "flex";
+    divContainerQuantidadeDeleteSeleçao.style.alignItems = "center";
+
+  }
+}
+
+window.addEventListener("load", aplicarMedia);
+
+window.addEventListener("resize", aplicarMedia);
